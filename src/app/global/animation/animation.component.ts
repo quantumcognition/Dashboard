fa2d65f09935d42f70aa38f9ec3ac970e0f3e6d8
Anantha Krishnan Ramasubramaniam <space_cowboy@Ananthas-MacBook-Pro.local>
2020-02-28 11:29:04 -0600

    Spinner

--- src/app/global/animation/animation.component.ts
+++ src/app/global/animation/animation.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { animation } from '@angular/animations';
 
 @Component({
   selector: 'app-animation',
   templateUrl: './animation.component.html',
-  styleUrls: ['./animation.component.scss']
+  styleUrls: ['./animation.component.scss','./animation.component.css']
 })
 export class AnimationComponent implements OnInit {
 
